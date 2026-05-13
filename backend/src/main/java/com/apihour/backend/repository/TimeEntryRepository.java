990509ee4cafbf63332d3d6df352098f93b1e25d
Kamicron <ludovic.chevroulet@outlook.fr>
2026-05-13 20:51:17 +0200

    feat(backend): Add findByUserIdAndStartTimeBetween to TimeEntryRepository

--- backend/src/main/java/com/apihour/backend/repository/TimeEntryRepository.java
+++ backend/src/main/java/com/apihour/backend/repository/TimeEntryRepository.java
@@ -4,9 +4,12 @@ import com.apihour.backend.model.TimeEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface TimeEntryRepository extends JpaRepository<TimeEntry, String> {
   List<TimeEntry> findByUserId(String userId);
+
+  List<TimeEntry> findByUserIdAndStartTimeBetween(String userId, Date startDate, Date endDate);
 }
