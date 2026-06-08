15625d21b48b63c515ea2a2ba7afcd45448f398e
Ludovic Chevroulet <ludovic@valuemycar.fr>
2026-06-08 08:39:58 +0200

    fix(frontend): vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['apihour2.pi-cto.top']
+  },
+  preview: {
+    allowedHosts: ['apihour2.pi-cto.top']
+  },
   resolve: { extensions: ['.js', '.jsx'] }
 })
